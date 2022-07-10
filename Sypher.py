13f3c8fd52225d2cb0f3218b7e65cea15d8dae48
Bpem9 <itimeq@gmail.com>
2022-07-10 16:37:31 +0300

    Repo remsatering

--- Sypher.py
+++
@@ -1,7 +0,0 @@
-
-#frequency = 0.0005
-text = 'd285d1bad1aed291d299d2a2d2b6'
-#def decrypt(text, key):
-#    for i in text:
-
-print(chr(int(0x21)+5))
--- Training2.py
+++
@@ -1,3 +0,0 @@
-def in_array(array1, array2):
-    return ([word for word in array1 if any(word in sub for sub in array2)])
-print(in_array(["arp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]))
