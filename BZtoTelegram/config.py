efa04320a2175c2203bbba3290263631b593934d
Bpem9 <106831482+Bpem9@users.noreply.github.com>
2022-07-10 20:45:53 +0300

    Refactoring my repos

--- BZtoTelegram/config.py
+++
@@ -1,9 +0,0 @@
-URL = 'https://***/real-estate/houses-and-villas-rent/lemesos-district-limassol/?type_view=line&ordering=newest&price_max=1500'
-HEADERS={
-    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36 OPR/88.0.4412.53',
-    'accept':'*/*'
-}
-ID_REG = r'/adv/([0-9]{7})(\_)(.*)'
-HOUSE_TYPE_REG = r'(\s?)(.*)(\s)to rent$'
-HOST = 'https://***'
-tg_bot_token = '***'
